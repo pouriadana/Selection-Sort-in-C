a0c8cdb995bea2bf2f7cad69d83444d8bdfc5639
pouria1337 <pouriadana73@gmail.com>
2023-04-03 09:42:25 +0330

    Add working code

---
+++ selectionsort_noptr_nonrec.c
@@ -0,0 +1,48 @@
+/*******************************
+* working selection sort
+* NO POINTER
+* NO RECURSION
+* April 3, 2023
+* Pouria Dana
+*******************************/
+#include <stdio.h>
+#define ARR_LEN 5
+
+void selection_sort(int [], int );
+
+int main()
+{
+    int arr[ARR_LEN] = {0};
+    int i;
+
+    printf("--> ");
+    for (i = 0; i < ARR_LEN; i++)
+    {
+        scanf(" %d", &arr[i]);
+    }
+
+    selection_sort(arr, ARR_LEN);
+    
+    for (i = 0; i < ARR_LEN; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
+
+void selection_sort(int a[], int n)
+{
+    int i, j, temp;
+    for (; n > 1; n--)
+    {
+        j = 0;
+        for (i = 1; i < n; i++)
+        {
+            if (a[i] > a[j])
+                j = i;
+        }
+        temp = a[n-1];
+        a[n-1] = a[j];
+        a[j] = temp;
+    }
+}
